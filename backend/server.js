8c97cc834b4b27dd9191a1dd5cea88da749bf8a0
Bejan-Silviu <silviubejan.gh@gmail.com>
2024-05-08 17:13:21 +0300

    Added folders

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const userRouter = require('./routes/userRouter')
+const nodeRouter = require('./routes/nodeRouter')
+const path = require('path')
+
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+//Routes
+app.use('/user',userRouter)
+app.use('/api/notes',nodeRouter)
+
+
+//connection
+const URI = process.env.DATABASE
+mongoose.connect("mongodb+srv://bejangherasimsilviu20:AJCu-96JWLPh_St@notesapp.jylo01p.mongodb.net/?retryWrites=true&w=majority&appName=NotesApp");
+
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) =>{
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    });
+}
+
+
+//listen server
+const PORT = process.env.PORT
+app.listen(PORT,()=>console.log(`server is listining on port ${PORT}` ))
+
+
