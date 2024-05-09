b06a1500509c3b9d85eaf41b9f2a8808174b73c6
Bejan-Silviu <silviubejan.gh@gmail.com>
2024-05-09 15:48:48 +0300

    Vercel test

--- backend/server.js
+++ backend/server.js
@@ -6,42 +6,45 @@ const userRouter = require('./routes/userRouter')
 const nodeRouter = require('./routes/nodeRouter')
 const path = require('path')
 
-
 const app = express();
 
-app.use(express.json())
-app.use(cors())
-
-//Routes
-app.use('/user',userRouter)
-app.use('/api/notes',nodeRouter)
 
 app.use(cors({
     origin: ["https://my-simpre-project.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
 }));
-//connection
-const URI = process.env.DATABASE
-mongoose.connect(URI,err=>{
-    if(err) throw err;
-    console.log('connecte to mongodb')
 
-})
 
-app.get("/",(req,res)=>{
-    res.json("Hello!");
-})
-if(process.env.NODE_ENV === 'production'){
+app.use(express.json());
+
+
+app.use('/user', userRouter);
+app.use('/api/notes', nodeRouter);
+
+
+const URI = process.env.DATABASE;
+mongoose.connect(URI, err => {
+    if (err) throw err;
+    console.log('Connected to MongoDB');
+});
+
+
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    app.get('*', (req, res) =>{
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+   
     });
 }
 
 
-//listen server
-const PORT = process.env.PORT
-app.listen(PORT,()=>console.log(`server is listining on port ${PORT}` ))
+app.get("/", (req, res) => {
+    res.json("Hello!");
+});
 
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
