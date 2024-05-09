dde627f7eed3f7c1c213bc8dd86e390407b3a475
Bejan-Silviu <silviubejan.gh@gmail.com>
2024-05-09 19:37:04 +0300

    Vercel test

--- frontend/src/components/notes/EditNotes.js
+++ frontend/src/components/notes/EditNotes.js
@@ -1,118 +1,103 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useNavigate,useParams } from 'react-router-dom'
+import axios from 'axios'
+// import './nav.css'
 
 function EditNote() {
-  const { id } = useParams();
-  const [note, setNote] = useState({
-    title: '',
-    content: '',
-    date: '',
-    id: '',
-  });
-  const [error, setError] = useState('');
+  const {id} = useParams()
 
-  const history = useNavigate();
+  const [note, setNote] = useState({
+    title: " ",
+    content: " ",
+    date: " ",
+    id: " "
+
+  })
+
+  const history = useNavigate()
 
   useEffect(() => {
     const getNote = async () => {
-      const token = localStorage.getItem('tokenStore');
+      const token = localStorage.getItem('tokenStore')
       if (id) {
-        try {
-          const res = await axios.get(`https://my-simpre-project.vercel.app/api/notes/${id}`, {
-            headers: { Authorization: token },
-          });
-          setNote({
-            title: res.data.title,
-            content: res.data.content,
-            date: new Date(res.data.date).toLocaleDateString(),
-            id: res.data._id,
-          });
-        } catch (err) {
-          setError(err.message);
-        }
+        const res = await axios.get(`https://my-simpre-project.vercel.app/api/notes/${id}`, {
+          headers: { Authorization: token }
+        })
+     setNote({
+      title: res.data.title,
+      content : res.data.content,
+      date: new Date(res.data.date).toLocaleDateString() ,
+      id:res.data._id
+     })
       }
-    };
+    }
 
-    getNote();
-  }, [id]);
+    getNote()
+  },[id])
 
-  const onChangeInput = e => {
+  const onChaneInput = e => {
     const { name, value } = e.target;
-    setNote({ ...note, [name]: value });
-  };
+    setNote({ ...note, [name]: value })
+  }
 
   const editNote = async e => {
-    e.preventDefault();
+    
+    e.preventDefault()
     try {
-      const token = localStorage.getItem('tokenStore');
+      const token = localStorage.getItem('tokenStore')
       if (token) {
-        const { title, content, date, id } = note;
+        const { title, content, date,id } = note;
         const newNote = {
-          title,
-          content,
-          date,
-        };
+          title, content, date
+        }
 
         await axios.put(`https://my-simpre-project.vercel.app/api/notes/${id}`, newNote, {
-          headers: { Authorization: token },
-        });
+          headers: { Authorization: token }
+        })
 
-        history.push('/');
+        return history.push('/')
+       
+
       }
+
     } catch (err) {
-      setError(err.message);
+      window.location.href = "/";
     }
-  };
+  }
+
+
 
   return (
     <div className="create-note">
       <h2>Edit note</h2>
 
-      {error && <div className="error">{error}</div>}
-
-      <form onSubmit={editNote} autoComplete="off">
+      <form onSubmit={editNote} autoComplete='off'>
         <div className="row">
           <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            value={note.title}
-            required
-            onChange={onChangeInput}
+          <input type="text" id='title' name="title"
+            value={note.title} required onChange={onChaneInput}
           />
         </div>
 
         <div className="row">
           <label htmlFor="content">Content</label>
-          <textarea
-            cols="30"
-            rows="10"
-            type="textarea"
-            id="content"
-            name="content"
-            value={note.content}
-            required
-            onChange={onChangeInput}
+          <textarea cols="30" rows="10" type="textarea" id='content' name="content"
+            value={note.content} required
+            onChange={onChaneInput}
           />
         </div>
 
+        <label htmlFor="date">Date:{note.date}</label>
         <div className="row">
-          <label htmlFor="date">Date:</label>
-          <input
-            type="date"
-            id="date"
-            name="date"
-            value={note.date}
-            onChange={onChangeInput}
+          <input type="date" id='date' name="date"
+         onChange={onChaneInput}
           />
         </div>
 
-        <button type="submit">Save</button>
+        <button type='submit'>save</button>
       </form>
     </div>
-  );
+  )
 }
 
-export default EditNote;
+export default EditNote
